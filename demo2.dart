715acdec1622b83ed75afe82b6524c55d1cb7996
srikar <srikarkurma5@gmail.com>
2024-08-27 14:35:17 +0530

    demo1

---
+++ demo2.dart
@@ -0,0 +1,14 @@
+void main()
+{
+print(Constants.name);
+print(Constants.bye);
+print(Constants.givemeSomeValue());
+}
+class  Constants{
+  static String name="hi hello";
+  static String bye="bye";
+  static int givemeSomeValue()
+  {
+    return 10;
+  }
+}
